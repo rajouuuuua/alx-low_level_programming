ed3fd1bd78971876c6a91b341aaa18bb44da6921
Rajae <rajaarmili@gmail.com>
2023-04-06 10:02:50 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _puts_recursion - print string
- * @s: print the string
- * Return: 0
+ *_puts_recursion - print string
+ *@s: print the string
+ *Return: 0
  */
 void _puts_recursion(char *s)
 {
